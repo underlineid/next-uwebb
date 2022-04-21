a9f3c3d06acba111da1ce4dcf60b65426c8468dc
unknown <box.underline@gmail.com>
2022-04-22 01:25:05 +0700

    no 404 visual in unauth

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Page404 } from 'components'
 import { useRouter } from 'next/router'
 import { getCookie } from 'helper/util'
 
 export default function NotFound() {
-  const { pathname: path, replace } = useRouter()
-  useEffect(() => {
-    const isLogin = getCookie('is-login')
-    const unauthPage = ['/login', '/register']
-    const inUnauth = unauthPage.indexOf(path) < 0
-    if (!isLogin && inUnauth) replace('/')
-  }, [path, replace])
+  const { pathname: path } = useRouter()
+  const isLogin = getCookie('is-login')
+  const unauthPage = ['/login', '/register']
+  const inUnauth = unauthPage.indexOf(path) < 0
+  if (!isLogin && inUnauth) return ''
   return <Page404 />
 }
