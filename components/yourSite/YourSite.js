a26f3a59791128ea817dd11a155970018a1a611b
unknown <box.underline@gmail.com>
2022-04-30 02:20:01 +0700

    try to adding Link Preview

--- components/yourSite/YourSite.js
+++ components/yourSite/YourSite.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { getLinkPreview } from 'link-preview-js'
 import style from './YourSite.module.scss'
 
 const SiteThumbnail = ({
   site_name: name,
   status = 'active',
-  site_url: url
+  site_url: url,
+  site_notion: notionUrl
 }) => {
+  const [img, setImg] = useState(false)
+
+  console.log(name, notionUrl)
+
+  const gets = useCallback(async () => {
+    // await getLinkPreview('https://www.youtube.com/watch?v=MejbOFk7H6c', {
+    //   imagesPropertyType: 'og', // fetches only open-graph images
+    //   headers: {
+    //     'user-agent': 'googlebot', // fetches with googlebot crawler user agent
+    //     'Accept-Language': 'fr-CA' // fetches site for French language
+    //     // ...other optional HTTP request headers
+    //   },
+    //   timeout: 1000
+    // }).then((data) => console.debug('Link Preview: ', data))
+  }, [notionUrl])
+
+  useEffect(() => {
+    if (notionUrl) gets()
+  }, [notionUrl, gets])
+
   return (
     <div className={style.siteThumbnail}>
       <div className={style.siteImage} />
