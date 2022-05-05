03c959fc8125bd3887ce99a24c7fb8f5dd0da9da
unknown <box.underline@gmail.com>
2022-05-05 15:00:38 +0700

    please god...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,24 @@
 import 'antd/dist/antd.css'
 import '../styles/global.scss'
 
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-coy.css'
+
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+
 // global style overrides for notion
 // import '../styles/notion.css'
 import '../styles/notion.css'
 
 // global style overrides for prism theme (optional)
 import '../styles/prism-theme.css'
-
-// core styles shared by all of react-notion-x (required)
-import '../react-notion-x/src/styles.css'
-
-// used for rendering equations (optional)
-import '../katex/dist/katex.min.css'
-
-// used for code syntax highlighting (optional)
-import '../prismjs/themes/prism-coy.css'
 
 import '../styles/antd-custom.scss'
 
