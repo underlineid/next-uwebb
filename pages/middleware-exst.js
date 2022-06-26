8398fd532ac0d26cd43e87f9ce2b9ccc446514dc
azizditya@gmail.com <azizditya@gmail.com>
2022-06-27 05:07:32 +0700

    how middleware different

--- pages/middleware-exst.js
+++ pages/middleware-exst.js
@@ -11,43 +11,30 @@ export default function middleware(req: NextRequest, res = NextResponse) {
       ? hostname
           .replace(`.vercel.pub`, '')
           .replace(`.platformize.vercel.app`, '')
-      : hostname.replace('.localhost:3000', '')
+      : hostname.replace('.localhost:9090', '')
 
   console.log('--------------MIDDLEWARE WORKER STARTED NEW-------------------')
-  // console.log('REQUEST: ', req)
-  // console.log('RESPONSE: ', res)
   console.log('HOSTNAME: ', hostname)
   console.log('PATHNAME: ', pathname)
   console.log('CURRENT HOST: ', currentHost)
-
-  const rewriteDashboard = () => {
-    url.pathname = `/dashboard${pathname}`
-    return NextResponse.rewrite(url)
-  }
 
   if (!hostname)
     return new Response(null, {
       status: 400,
       statusText: 'No hostname found in request headers'
     })
-
   if (pathname.startsWith('/_sites')) return new Response(null, { status: 404 })
-
-  if (!pathname.includes('.')) {
-    if (currentHost === 'dashboard') rewriteDashboard()
-  }
-
-  if (currentHost.includes('dashboard.')) return rewriteDashboard()
-
-  if (hostname === 'localhost:9090' || hostname === 'uwebb.vercel.app') {
-    url.pathname = `/home${pathname}`
+  if (currentHost.includes('dashboard.')) {
+    url.pathname = `/dashboard${pathname}`
     return NextResponse.rewrite(url)
   }
-
-  // console.log('MIDDLEWARE: ', { currentHost, pathname })
-  url.pathname = `/_sites/${currentHost}/`
-  // if (pathname === '/') url.pathname = `/_sites/${currentHost}/`
-  // else url.pathname = `/_sites/${currentHost}[pageid].tsx`
+  if (hostname === 'localhost:9090' || hostname === 'uwebb.vercel.app')
+    url.pathname = `/home${pathname}`
+  url.pathname = `/_sites/${hostname}${pathname}`
+  // else {
+  //   if (pathname === '/') url.pathname = `/_sites/index.js`
+  //   else url.pathname = `/_sites/[pageId].tsx`
+  // }
 
   console.log('REWRITTEN URL :', url)
   console.log('--------------MIDDLEWARE WORKER ENDED NEW-------------------')
--- pages/middleware-jojo.ts
+++ pages/middleware-jojo.ts
@@ -1,12 +1,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export default function middleware(req: NextRequest, res: NextResponse) {
+export default function middleware(req: NextRequest, res = NextResponse) {
   const { pathname } = req.nextUrl
   const url = req.nextUrl.clone()
   const hostname = req.headers.get('host')
-
-  let consoleMarker = 0
 
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
@@ -15,47 +13,44 @@ export default function middleware(req: NextRequest, res: NextResponse) {
           .replace(`.platformize.vercel.app`, '')
       : hostname.replace('.localhost:3000', '')
 
-  console.log('MIDDLEWARE WORKER STARTED')
-  console.log('REQUEST: ', req)
-  console.log('RESPONSE: ', res)
+  console.log('--------------MIDDLEWARE WORKER STARTED NEW-------------------')
+  // console.log('REQUEST: ', req)
+  // console.log('RESPONSE: ', res)
   console.log('HOSTNAME: ', hostname)
   console.log('PATHNAME: ', pathname)
   console.log('CURRENT HOST: ', currentHost)
 
-  if (!hostname) {
-    consoleMarker = 26
+  const rewriteDashboard = () => {
+    url.pathname = `/dashboard${pathname}`
+    return NextResponse.rewrite(url)
+  }
+
+  if (!hostname)
     return new Response(null, {
       status: 400,
       statusText: 'No hostname found in request headers'
     })
-  }
 
-  if (pathname.startsWith('/_sites')) {
-    console.log('IN LINE 31')
-    return new Response(null, { status: 404 })
-  }
+  if (pathname.startsWith('/_sites')) return new Response(null, { status: 404 })
 
   if (!pathname.includes('.')) {
-    consoleMarker = 39
-    if (currentHost === 'dashboard') {
-      url.pathname = `/dashboard${pathname}`
-      return NextResponse.rewrite(url)
-    }
+    if (currentHost === 'dashboard') rewriteDashboard()
   }
+
+  if (currentHost.includes('dashboard.')) return rewriteDashboard()
 
   if (hostname === 'localhost:9090' || hostname === 'uwebb.vercel.app') {
     url.pathname = `/home${pathname}`
     return NextResponse.rewrite(url)
   }
 
-  url.pathname = `/_sites/${currentHost}${pathname}`
-  consoleMarker = 47
-
-  console.log('CONSOLE MARKER: ', consoleMarker)
+  // console.log('MIDDLEWARE: ', { currentHost, pathname })
+  url.pathname = `/_sites/${currentHost}/`
+  // if (pathname === '/') url.pathname = `/_sites/${currentHost}/`
+  // else url.pathname = `/_sites/${currentHost}[pageid].tsx`
 
   console.log('REWRITTEN URL :', url)
-  console.log('MIDDLEWARE WORKER ENDED')
-  console.log('-----------------------------------------')
+  console.log('--------------MIDDLEWARE WORKER ENDED NEW-------------------')
 
   // return new Response(`This NextRouting BYPASSED by middleware. url: ${url}`)
   return NextResponse.rewrite(url)
