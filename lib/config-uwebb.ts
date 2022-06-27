d95cd2750119e9df48d48d4bd1d637b9a0370535
azizditya@gmail.com <azizditya@gmail.com>
2022-06-27 14:02:40 +0700

    del

--- lib/config-uwebb.ts
+++
@@ -1,5 +0,0 @@
-// import { getUwebbConfig } from './get-config-value'
-
-// export const description: string = getUwebbConfig('description', null)
-
-// export const name: string = getUwebbConfig('name')
